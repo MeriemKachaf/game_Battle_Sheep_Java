015d3c43b0b0d1f8e5f3276fc922763d8cebc68c
Meriem <meriemkachaf124@gmail.com>
2025-12-15 09:55:06 +0100

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.game_battle_sheep {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.game_battle_sheep to javafx.fxml;
+    exports com.example.game_battle_sheep;
+}
